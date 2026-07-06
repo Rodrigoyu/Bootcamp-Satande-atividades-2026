2de13dee076b34bff7b9753703c173f5b3dea477
rodrigo <rodrigoj1478@gmail.com>
2026-07-06 11:10:35 -0300

    Atividade 2 feita

--- src/atividade2/Carro.java
+++ src/atividade2/Carro.java
@@ -10,6 +10,10 @@ public class Carro {
 
     }
 
+    public void getVelocidade() {
+        System.out.println(velocidade + "KM");
+    }
+
     public void ligar(){
         System.out.println("O carro esta ligado");
         this.chave = 1;
@@ -18,13 +22,18 @@ public class Carro {
     }
 
     public void setMacha(){
-        this.macha =this.macha + 1;
-        System.out.println("mudou a macha para " + this.macha);
+        if (this.chave != 1){
+            System.out.println("carro desligador");
+            return;
+        }
+
+        this.macha += 1;
+
     }
 
     public void desligar(){
         if (this.velocidade == 0) {
-            System.out.println("carro desligado");
+            System.out.println("carro desligador");
             this.chave = 0;
             this.velocidade = 0;
             this.macha = 0;
@@ -34,34 +43,71 @@ public class Carro {
         }
     }
 
-    public void acelerar(){
-        if (this.chave == 1 && this.macha == 1 ){
-            this.velocidade = this.velocidade + 1;
-            System.out.println("seu carro acelorou"+ this.velocidade + "KM");
-            if (this.velocidade == 20){
-                this.velocidade = this.velocidade -1;
 
-            }
+    public void acelerar(){
+        if (this.chave != 1){
+            System.out.println("carro desligador");
+            return;
         }
+
+        int limiteDeVelocidade = limite(this.macha);
+
+        if (this.velocidade >= limiteDeVelocidade){
+            System.out.println("você tem que muda de macha para acelera mais");
+            return;
+        }
+
+        this.velocidade += 1;
+        System.out.println("seu carro acelerou "+ this.velocidade+ " KM");
+
+    }
+
+    public int limite(int macha){
+        return switch (macha){
+            case 1 -> 20;
+            case 2 -> 40;
+            case 3 -> 60;
+            case 4 -> 80;
+            case 5 -> 100;
+            case 6 -> 120;
+            default -> 0;
+        };
     }
 
     public void desacelerar(){
-        if (this.chave == 1 && this.velocidade > 0){
-            this.velocidade = this.velocidade - 1;
-            System.out.println("seu carro diminuio a velocidade em "+ this.velocidade + "KM");
-        }else if (this.chave == 0){
-            System.out.println("seu carro esta desligado");
-        }else{
-            System.out.println("Seu carro ja esta parado");
+        if (this.chave != 1){
+            System.out.println("carro desligador");
+            return;
         }
+
+        if (this.velocidade == 0){
+            System.out.println("seu carro ja esta parado");
+            this.velocidade = 0;
+            return;
+        }
+
+        this.velocidade -=1;
+        System.out.println("seu carro desacelereou para " + this.velocidade + "KM");
+
+
+
     }
 
     public void volante(String valor){
-        if (this.chave == 1){
-            if (valor.equalsIgnoreCase("esquerda")){
-               System.out.println("virou a " + valor);
-            }
+        if (this.chave != 1){
+            System.out.println("carro desligador");
+            return;
         }
+
+        if (this.velocidade > 40){
+            System.out.println("Diminua a velocidade para muda de direção ");
+            return;
+        }
+        if (this.velocidade == 0 ){
+            System.out.println("seu carro esta parado");
+            return;
+        }
+        System.out.println("Seu carro mudou para " + valor);
     }
 
 
--- src/atividade2/Main.java
+++ src/atividade2/Main.java
@@ -1,18 +1,54 @@
 package atividade2;
 
+import java.util.Scanner;
+
 public class Main {
     public  static void main(String[] args){
         Carro c = new Carro();
-
-        c.ligar();
-        c.setMacha();
-        c.acelerar();
-        c.acelerar();
-        c.desacelerar();
-        c.desacelerar();
-        c.volante("direita");
-        c.desligar();
-        c.acelerar();
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < 8; ){
+            System.out.println("=======================================");
+            System.out.println("escolha uma das opçoes a seguir");
+            System.out.println("1 - ligar");
+            System.out.println("2 - desligar");
+            System.out.println("3 - acelera");
+            System.out.println("4 - desacelera");
+            System.out.println("5 - volante");
+            System.out.println("6 - verifica velosidade");
+            System.out.println("7 - macha");
+            System.out.println("8 - sair");
+            int opcao = sc.nextInt();
+            System.out.println("=======================================");
+            switch (opcao){
+                case 1:
+                    c.ligar();
+                    break;
+                case 2:
+                    c.desligar();
+                    break;
+                case 3:
+                     c.acelerar();
+                     break;
+                case 4:
+                    c.desacelerar();
+                    break;
+                case 5:
+                    System.out.println("digite a direcão:");
+                    String valor = sc.nextLine();
+                    c.volante(valor);
+                    break;
+                case 6:
+                    c.getVelocidade();
+                    break;
+                case 7:
+                    c.setMacha();
+                    break;
+                case 8:
+                default:
+                    System.out.println("Finalizando...");
+                    i = 9;
+            }
+        }
 
     }
 }
